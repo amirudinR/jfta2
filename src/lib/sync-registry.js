7bb5b6249dee919d95a0adf697ca4bb168ea37cb
Amirudin Ridwan <amirudinridwan99@gmail.com>
2026-09-15 13:59:41 +0700

    fix: ReferenceError uid is not defined pada path sync dinamis

--- src/lib/sync-registry.js
+++ src/lib/sync-registry.js
@@ -92,17 +92,17 @@ export const SYNC_STORES = [
   { key: 'ankichou-level', path: (uid) => `users/${uid}/hh/level`, merge: lww },
   ...dynamic(
     (m) => `hh2-checked-${m}`,
-    (m) => `users/${uid}/hh/checked/${m}`,
+    (u, mk) => `users/${u}/hh/checked/${mk}`,
     mergeChecked,
   ),
   ...dynamic(
     (m) => `hh2-hist-${m}`,
-    (m) => `users/${uid}/hh/history/${m}`,
+    (u, mk) => `users/${u}/hh/history/${mk}`,
     mergeDays,
   ),
   ...dynamic(
     (m) => `hh2-custom-${m}`,
-    (m) => `users/${uid}/hh/custom/${m}`,
+    (u, mk) => `users/${u}/hh/custom/${mk}`,
     mergeCustom,
   ),
 ]
